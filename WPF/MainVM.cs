6c8cd879efbca01eef307af2b6bbd3938f7948c1
Sergei Vysokov <svysokov@gmail.com>
2020-12-25 14:59:28 +0300

    Removed empty constructor

--- WPF/MainVM.cs
+++ WPF/MainVM.cs
@@ -36,10 +36,6 @@ namespace WPF
 
             }
         }
-        MainVM()
-        {
-
-        }
         public MainVM(Dispatcher mainDispatcher)
         {
             cDispatcher = mainDispatcher;
